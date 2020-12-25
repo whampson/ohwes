8df5b6bafff80fd10498a051b8b31c2218eb763b
Wes Hampson <thehambone93@gmail.com>
2020-12-24 22:06:28 -0600

    Fill out remaining IRQs

--- include/ohwes/irq.h
+++ include/ohwes/irq.h
@@ -23,8 +23,19 @@
 #ifndef __IRQ_H
 #define __IRQ_H
 
+#define IRQ_TIMER       0
 #define IRQ_KEYBOARD    1
 #define IRQ_SLAVE_PIC   2
+#define IRQ_COM2        3
+#define IRQ_COM1        4
+#define IRQ_LPT2        5
+#define IRQ_FLOPPY      6
+#define IRQ_LPT1        7
+#define IRQ_RTC         8
+#define IRQ_MOUSE       12
+#define IRQ_FPU         13
+#define IRQ_ATA1        14
+#define IRQ_ATA2        15
 #define NUM_IRQ         16
 
 #ifndef __ASSEMBLY__
