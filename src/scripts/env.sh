9123435d34b69eb2dc9e3d3dac1274b874638393
Wes Hampson <WesleyHampson@gmail.com>
2022-07-04 16:05:30 -0700

    More makefile tweakery

--- src/scripts/env.sh
+++ src/scripts/env.sh
@@ -10,22 +10,16 @@
 # To use, run `source env.sh` in your shell.
 #===============================================================================
 
-# !!! This file must live in <root>/scripts/ or _OSROOT will be defined incorrectly !!!
+# !!! This file must live in <root>/src/scripts/ or _OSROOT will be defined incorrectly !!!
 
-export _OSROOT=$(dirname $(dirname $(realpath ${BASH_SOURCE[0]})))
+export _OSROOT=$(dirname $(dirname $(dirname $(realpath ${BASH_SOURCE[0]}))))
 export _BINROOT=$_OSROOT/bin
 export _OBJROOT=$_OSROOT/obj
 export _SRCROOT=$_OSROOT/src
-export _SCRIPTS=$_OSROOT/scripts
-export _TOOLSRC=$_OSROOT/tools
+export _SCRIPTS=$_SRCROOT/scripts
+export _TOOLSRC=$_SRCROOT/tools
 
-# TODO: set these based on script args
-export _DEBUG=1
-
-QEMU="/c/Program Files/qemu"    # TODO: this should go somewhere local
-BINUTILS=$_OSROOT/lib/i686-elf-tools/bin
-OSTOOLS=$_TOOLSRC/bin
-export PATH=$PATH:$BINUTILS:$QEMU:$OSTOOLS:$_SCRIPTS
+export DEBUG=1
 
 echo "Setting build variables..."
 echo "   _OSROOT = $_OSROOT"
@@ -33,13 +27,20 @@ echo "  _BINROOT = $_BINROOT"
 echo "  _OBJROOT = $_OBJROOT"
 echo "  _SRCROOT = $_SRCROOT"
 echo "  _SCRIPTS = $_SCRIPTS"
-echo "  _TOOLSRC = $_TOOLSRC"
-echo "    _DEBUG = $_DEBUG"
-echo 
+
+QEMU="/c/Program Files/qemu"    # TODO: this should go somewhere repository-local
+BINUTILS=$_OSROOT/lib/i686-elf-tools/bin
+TOOLSBIN=$_BINROOT/tools
+export PATH=$PATH:$BINUTILS:$QEMU:$TOOLSBIN:$_SCRIPTS
+
 echo "Updating PATH..."
-echo "  PATH += $BINUTILS"
-echo "  PATH += $QEMU"
-echo "  PATH += $OSTOOLS"
-echo "  PATH += $_SCRIPTS"
+echo "     PATH += $BINUTILS"
+echo "     PATH += $QEMU"
+echo "     PATH += $OSTOOLS"
+echo "     PATH += $_SCRIPTS"
+
+if [ -z "$DEBUG" ]; then
+    echo "Debug build."
+fi
 
 echo "All set!"
