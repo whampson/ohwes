4438f63995e20c2bfd7f99952b02454df35f6a96
Wes Hampson <WesleyHampson@gmail.com>
2022-07-09 23:01:52 -0700

    Begin 'fatfs' tool for manipulaing fat12 disks

--- src/tools/fatfs/main.cpp
+++ src/tools/fatfs/main.cpp
@@ -1,7 +1,26 @@
 #include <cstdio>
+#include "fat.hpp"
+#include "image.hpp"
 
-int main()
+#define ERROR(msg)                                      \
+do {                                                    \
+    fprintf(stderr, "fatfs: error: " msg);              \
+} while (0)
+
+#define ERRORF(fmt, ...)                                \
+do {                                                    \
+    fprintf(stderr, "fatfs: error: " fmt, __VA_ARGS__); \
+} while (0)
+
+int main(int argc, char **argv)
 {
-    printf("Hello, world!\n");
+    if (argc == 0) {
+        ERROR("missing argument");
+        return 1;
+    }
+
+    FatImage *img = FatImage::Load(argv[1]);
+    
+    delete img;
     return 0;
 }
