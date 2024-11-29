9f97f62321f98821fcfe580def0db319af2c35d2
Wes Hampson <WesleyHampson@gmail.com>
2024-11-29 15:40:58 -0800

    explicity specifiy 'pentium' arch

--- ohwes.mk
+++ ohwes.mk
@@ -1,3 +1,6 @@
+# architecture
+ARCH        := pentium
+
 # includes
 INCLUDES    := include
 
@@ -15,13 +18,13 @@ SUBMAKEFILES := $(addsuffix /Module.mk,${MODULES})
 
 # use cross-compiler toolchain
 PREFIX     := i686-elf-
-AR         := $(PREFIX)ar
-AS         := $(PREFIX)gcc
-CC         := $(PREFIX)gcc
-LD         := $(PREFIX)gcc
-OBJCOPY    := $(PREFIX)objcopy
+AR         := ${PREFIX}ar
+AS         := ${PREFIX}gcc
+CC         := ${PREFIX}gcc
+LD         := ${PREFIX}gcc
+OBJCOPY    := ${PREFIX}objcopy
 
 # flags, etc.
 ARFLAGS    := -rcsv
-CFLAGS     += -std=c11 -include compiler.h
+CFLAGS     += -std=c11 -include compiler.h -march=${ARCH}
 LDFLAGS    += -nostdlib -lgcc
